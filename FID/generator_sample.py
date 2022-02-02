2c920b9e7675f9d28430071114dc62448532e5af
chenkaixu@lab <chenkaixusan@gmail.com>
2022-02-02 07:27:07 +0000

    for the generator stata dict

--- FID/generator_sample.py
+++ FID/generator_sample.py
@@ -1,8 +1,24 @@
 #%%
+import sys
+sys.path.append("..")
+
+#%%
 import torch.nn as nn
 import torch 
+from utils.utils import *
 
 from models.wgan_div import Generator
+# %%
+Generator = Generator(channels=3).cuda()
 
+fixed_z = tensor2var(torch.randn(1000, 100)) # （*, 100）
 
+# Generator.eval()
+# fake_images = Generator(fixed_z)
+
+# %%
+checkpoint = torch.load("/GANs/WGAN-div-PyTorch-/checkpoint/1216_cifar10_bs64_matsumoto_1/10000.pth.tar")
+Generator.load_state_dict(checkpoint['G_state_dict'])
+# %%
+fake_images = Generator(fixed_z)
 # %%
